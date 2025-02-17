327b69b70175d561f82161097a19b7e01be6ad65
Chris_SB <christian.broendsholm@gmail.com>
2025-02-17 09:39:11 +0100

    Basic road and car function

--- bezier drift.js
+++ bezier drift.js
@@ -1,65 +1,36 @@
-let Points = []; 
-let A = {x:0, y:600}; //statisk
-let B = {x:600, y:500}; //statisk
+let points = []; 
+let A = {x:0, y:720}; //statisk
+let B = {x:1280, y:720/2}; //statisk
 let C = {x:0, y:0}; //dynamisk punkt
 let dragA, dragB;
-let scrollSpeed = 0
+let acc = {x:0, y:0}
+let origX = 0; let origY = 0;
+let carPos = {x:1280/2,y:720-200}
 function setup(){
-    createCanvas(window.innerWidth,window.innerHeight)
-    background('lightgrey')
+    createCanvas(1280,720)
 }
+
 document.addEventListener('contextmenu', event => event.preventDefault());
 let t = 0
 let IsHolding = false
 
 function draw(){
-    clear(); 
+    background('lightgrey')
     strokeWeight(5)
-    t+=0.005;
-  scrollSpeed +=1
-  A.x = -scrollSpeed;A.y = scrollSpeed;
-  B.x = -scrollSpeed;B.y = scrollSpeed;
-  C.x = -scrollSpeed;C.y = scrollSpeed;
-  translate(scrollSpeed,0)
-  if (keyIsDown(65)){
-    B.y += 20
-  }
-    if (keyIsDown(68)){
-    B.y -= 20
-  }
+    t += 0.003;
+    //origX--
+    //origY++
+    translate(origX, origY);
+//B.y=1280*sin(frameCount)
+
   
     if(IsHolding == true){
         C.x = mouseX
         C.y = mouseY
     }
     //Hvis man holder venstreklik kan man rykke punkterne
-    if(mouseIsPressed && mouseX > A.x - 10 / 2 &&
-        mouseX < A.x + 10 / 2 &&
-        mouseY > A.y - 10 / 2 &&
-        mouseY < A.y + 10 / 2 ){
-dragA = true
-        }
-        if(dragA === true){
-            A.x = mouseX
-            A.y = mouseY
-            if(!mouseIsPressed){
-                dragA=false;
-            }
-        }
-        if(mouseIsPressed && mouseX > B.x - 10 / 2 &&
-            mouseX < B.x + 10 / 2 &&
-            mouseY > B.y - 10 / 2 &&
-            mouseY < B.y + 10 / 2 ){
-    dragB = true
-            }
-            if(dragB==true){
-                B.x = mouseX
-                B.y = mouseY
-                if(!mouseIsPressed){
-                    dragB=false;
-                }
-            }
 
+//for (let t = 0; t<=1; t+=0.003){
     let dX = lerp(A.x,C.x,t)
     let dY = lerp(A.y,C.y,t)
     let eX = lerp(C.x,B.x,t)
@@ -67,51 +38,86 @@ dragA = true
     let gX = lerp(dX,eX,t)
     let gY = lerp(dY,eY,t)
     Pg = {x:gX, y:gY} //objekt for bezierkurvens punkter
-    Points.push(Pg)//sætter x- og y værdier i et array
+    points.push(Pg)//sætter x og y værdier i et array
+//}
+    //tegner en linje mellem points listens index x og y værdier -1 og den nuværende
+   for (let i = 8; i < points.length; i++) {
+    let slope = createVector(points[i].x-points[i-1].x, points[i].y-points[i-1].y)
+    stroke(80)
+    angleMode(DEGREES)
+    slope.setMag(80)
+    //line(points[i].x, points[i].y, points[i].x + slope.x , points[i].y + slope.y)
+    let normL = p5.Vector.rotate(slope,90)
+    let normR = p5.Vector.rotate(slope,-90)
+    strokeWeight(40)
+    line(points[i].x, points[i].y, points[i].x + normL.x , points[i].y + normL.y)
+    line(points[i].x, points[i].y, points[i].x + normR.x , points[i].y + normR.y)
+    //lav gule linjer til vejen
+    strokeWeight(4)
+    stroke('yellow')
+    let segment = i / points.length;
+    stroke(segment * 255, 255, (1 - segment) * 255);
+    slope.setMag(5)
+    //line(points[i].x, points[i].y, points[i].x + slope.x , points[i].y + slope.y)
+    let roadL = p5.Vector.rotate(slope,90)
+    let roadR = p5.Vector.rotate(slope,-90)
+        line(points[i-8].x + roadL.x, points[i-8].y + roadL.y, points[i].x + roadL.x , points[i].y + roadL.y)
+        line(points[i-8].x + roadR.x, points[i-8].y + roadR.y, points[i].x + roadR.x , points[i].y + roadR.y)
+   stroke(255)
+        slope.setMag(90)
+    let roadBL = p5.Vector.rotate(slope,90)
+    let roadBR = p5.Vector.rotate(slope,-90)     
+        line(points[i-8].x + roadBL.x, points[i-8].y + roadBL.y, points[i].x + roadBL.x , points[i].y + roadBL.y)
+        line(points[i-8].x + roadBR.x, points[i-8].y + roadBR.y, points[i].x + roadBR.x , points[i].y + roadBR.y)
+    }
 
-    //tegner en linje mellem Points listens index x og y værdier -1 og den nuværende
-   for (let i = 1; i < Points.length; i++) {
-    stroke('red')
-    line(Points[i-1].x,Points[i-1].y,Points[i].x,Points[i].y)
-   }
+if(t>1){
+    A.x = width
+    A.y = height
+}
+car();
+function car(){ 
+
+if (keyIsDown(65)){
+    carPos.x -= 20
+  }
+    if (keyIsDown(68)){
+    carPos.x += 20
+  }
+  if (keyIsDown(87)){
+    carPos.y -= 20
+  }
+    if (keyIsDown(83)){
+    carPos.y += 20
+  }
+rect(carPos.x-origX,carPos.y,40,100)
+}
+
+
+
 
    stroke('black')
-   line(A.x,A.y,C.x,C.y)
-   line(B.x,B.y,C.x,C.y)
+   //line(A.x,A.y,C.x,C.y)
+   //line(B.x,B.y,C.x,C.y)
    stroke('blue')
    //line(dX,dY,eX,eY)
 
    stroke('black')
    strokeWeight(2)
    fill('white')
-   circle(A.x,A.y,10)
-   circle(B.x,B.y,10)
-   circle(C.x,C.y,10)
+   //circle(A.x,A.y,10)
+   //circle(B.x,B.y,10)
+   //circle(C.x,C.y,10)
    fill('black')
-   circle(dX,dY,10)
-   circle(eX,eY,10)
+   //circle(dX,dY,10)
+   //circle(eX,eY,10)
    fill('white')
-   circle(gX,gY,10)
+   //circle(gX,gY,10)
 
 
    if (t >= 1) {
     //t = 0
-    //Points = []
+    //points = []
     //clear()
    }
-
-function mousePressed(){
-    if (mouseButton == "right"){
-        IsHolding = true
-    }
-}
-
-function mouseReleased(){
-    if (mouseButton == "right"){
-        clear();
-        Points = []
-        t = 0
-        IsHolding = false
-    }
-  }
 }
